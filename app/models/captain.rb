0dd89839f59bf7f2897f83e67839eab8e8e8744e
gabidavila <gabidavila@gmail.com>
2017-08-04 14:20:48 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.capitain_by_boat_classification(args)
+    self.joins(:boats => :classifications).where("classifications.name = ?", args[:classification_name])
+  end
+
+  def self.catamaran_operators
+    #TODO add this to my notebook
+    self.capitain_by_boat_classification(classification_name: "Catamaran")
+  end
+
+  def self.sailors
+    self.capitain_by_boat_classification(classification_name: "Sailboat").uniq
+  end
+
+  def self.talented_seamen
+    motorboats_captains = self.capitain_by_boat_classification(classification_name: "Motorboat")
+    sailors.where(id: motorboats_captains.pluck(:id))
+  end
+
+  def self.non_sailors
+    self.joins(:boats => :classifications).where("captains.id NOT IN (?)", sailors.pluck(:id)).uniq
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    longest_boat = self.joins(:boats).order("boats.length DESC").limit(1).pluck("boats.id")
+    self.joins(:boat_classifications).where("boat_classifications.boat_id = ?", longest_boat)
+  end
 end
